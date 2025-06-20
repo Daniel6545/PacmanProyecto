bd305a7209842ee6276ff7ef663b4395e335eef9
Dani Ruiz <Daniel.ruiz.soria@estudiantat.upc.edu>
2025-06-20 10:39:13 +0200

    Millores

--- juego.py
+++ juego.py
@@ -19,20 +19,48 @@ class Juego:
 
         self.running = True
 
-        self.blinky = Blinky()
-        self.fantasmas_group = pygame.sprite.Group(self.blinky)
-
         self.muro_grupo = pygame.sprite.Group()
+        self.coin_group = pygame.sprite.Group()
+        self.fantasmas_group = pygame.sprite.Group()
 
         self.clock = pygame.time.Clock()
         self.fuente = pygame.font.SysFont("Calibri", 20)
         self.puntuacion = 0
-        self.coin_group = pygame.sprite.Group()
+
         self.direccion_actual = (0, 0)
-
         self.mapa_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.crearmapa()  # Crea muros, monedas y Pac-Man
 
+        self.crearmapa()
+    def crearmapa(self):
+        for y, fila in enumerate(self.datos):
+            for x, celda in enumerate(fila):
+                pos = (x * self.tile_size, y * self.tile_size)
+
+                if celda in self.colores_paredes:
+                    muro = Muro(pos[0], pos[1])
+                    muro.image.fill(self.colores_paredes[celda])  # Pinta el muro con el color correcto
+                    self.muro_grupo.add(muro)
+
+                elif celda == "0":
+                    cx = x * self.tile_size + self.tile_size // 2
+                    cy = y * self.tile_size + self.tile_size // 2
+                    moneda = Coin(cx, cy)
+                    self.coin_group.add(moneda)
+
+                elif celda == "S":
+                    pygame.draw.circle(self.mapa_surface, Dorado,
+                                       (pos[0] + self.tile_size // 2, pos[1] + self.tile_size // 2), 5)
+
+                elif celda == "P":
+                    pac_x = x * self.tile_size + self.tile_size // 2
+                    pac_y = y * self.tile_size + self.tile_size // 2
+                    self.pacman = pacman(pac_x, pac_y)
+                    self.pacman_group = pygame.sprite.Group(self.pacman)
+                elif celda == "F":
+                    blinky_x = x * self.tile_size + self.tile_size // 2
+                    blinky_y = y * self.tile_size + self.tile_size // 2
+                    self.blinky = Blinky(self.muro_grupo, blinky_x, blinky_y)
+                    self.fantasmas_group.add(self.blinky)
     def update(self):
         keys = pygame.key.get_pressed()
 
@@ -67,31 +95,7 @@ class Juego:
             if event.type == pygame.QUIT:
                 self.running = False
 
-    def crearmapa(self):
-        for y, fila in enumerate(self.datos):
-            for x, celda in enumerate(fila):
-                pos = (x * self.tile_size, y * self.tile_size)
 
-                if celda in self.colores_paredes:
-                    muro = Muro(pos[0], pos[1])
-                    muro.image.fill(self.colores_paredes[celda])  # Pinta el muro con el color correcto
-                    self.muro_grupo.add(muro)
-
-                elif celda == "0":
-                    cx = x * self.tile_size + self.tile_size // 2
-                    cy = y * self.tile_size + self.tile_size // 2
-                    moneda = Coin(cx, cy)
-                    self.coin_group.add(moneda)
-
-                elif celda == "S":
-                    pygame.draw.circle(self.mapa_surface, Dorado,
-                                       (pos[0] + self.tile_size // 2, pos[1] + self.tile_size // 2), 5)
-
-                elif celda == "P":
-                    pac_x = x * self.tile_size + self.tile_size // 2
-                    pac_y = y * self.tile_size + self.tile_size // 2
-                    self.pacman = pacman(pac_x, pac_y)
-                    self.pacman_group = pygame.sprite.Group(self.pacman)
 
     def draw(self):
         self.ventana.blit(self.mapa_surface, (0, 0))
