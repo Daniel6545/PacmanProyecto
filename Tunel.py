df899f4266b53dee9fe8bff93df646afa0c97c1a
Dani Ruiz <Daniel.ruiz.soria@estudiantat.upc.edu>
2025-06-25 20:05:25 +0200

    Millores

--- Tunel.py
+++ Tunel.py
@@ -5,6 +5,3 @@ class tunel(Sprite):
     def __init__(self, x, y, tamaño):
         super().__init__()
         self.rect = pygame.Rect(x, y, tamaño, tamaño)
-
-    def contiene(self, objeto_rect):
-        return self.rect.colliderect(objeto_rect)
